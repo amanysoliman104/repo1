5e33f1993536fb139a74b1c21b2a1f6784b3eb7b
Unknown <unknown@example.com>
2018-03-17 16:58:37 -0700

    entites of project

---
+++ forums/main.py
@@ -0,0 +1,7 @@
+import Models
+
+member1 = Models.members("amany", 24)
+member2 = Models.members("anas", 20)
+post1   = Models.Posts("lesson1","undarstand this lesson ")
+post2   = Models.Posts("lesson2"," not undarstand this lesson")
+post3   =Models.Posts("lesson3","very nice lesson")
