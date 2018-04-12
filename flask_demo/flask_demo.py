a4da5b1ac65a3ac346e860922cfa3ab775b70761
Unknown <unknown@example.com>
2018-04-12 12:08:53 -0700

    view

--- flask_demo/flask_demo.py
+++ flask_demo/flask_demo.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask import Flask, render_template
+ 
 
 app = Flask(__name__)
-@app.route("/index1 ")
+@app.route("/index1")
 def home():
-    return "Hi there"
+    return render_template("index1.html")
+@app.route("/SayHello/<kasem>")
+def say_hello(name):
+    return f(" Hello {name} ")
 app.run()
